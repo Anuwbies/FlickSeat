7bc0e4460fdcba3a30ff11788e2f38648ceff2d4
Anuwbies <asi.rodas02@gmail.com>
2025-02-11 16:51:04 +0800

    Banana

--- app/src/main/java/com/example/flickseat/Signin.kt
+++ app/src/main/java/com/example/flickseat/Signin.kt
@@ -2,31 +2,24 @@ package com.example.flickseat
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class Signin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_signin)
 
-        val signIn_btn = findViewById<Button>(R.id.SignIn_button)
+        val txt_signin = findViewById<TextView>(R.id.txt_signin)
 
-        signIn_btn.setOnClickListener {
-            val intent = Intent(this, Signin::class.java)
-            startActivity(intent)
-        }
-
-        val signUp_btn = findViewById<Button>(R.id.SignUp_button)
-
-        signUp_btn.setOnClickListener {
+        txt_signin.setOnClickListener {
             val intent = Intent(this, Signup::class.java)
             startActivity(intent)
+            finish()
         }
     }
-
 }
--- app/src/main/java/com/example/flickseat/Signup.kt
+++ app/src/main/java/com/example/flickseat/Signup.kt
@@ -2,31 +2,24 @@ package com.example.flickseat
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class Signup : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_signup)
 
-        val signIn_btn = findViewById<Button>(R.id.SignIn_button)
+        val txt_signup = findViewById<TextView>(R.id.txt_signup)
 
-        signIn_btn.setOnClickListener {
+        txt_signup.setOnClickListener {
             val intent = Intent(this, Signin::class.java)
             startActivity(intent)
-        }
-
-        val signUp_btn = findViewById<Button>(R.id.SignUp_button)
-
-        signUp_btn.setOnClickListener {
-            val intent = Intent(this, Signup::class.java)
-            startActivity(intent)
+            finish()
         }
     }
-
 }

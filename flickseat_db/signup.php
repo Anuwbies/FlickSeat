bc651632c785cc580dbf609c1794a63e2adefd88
Anuwbies <asi.rodas02@gmail.com>
2025-03-11 22:00:28 +0800

    latest

--- flickseat_db/signup.php
+++ flickseat_db/signup.php
@@ -5,7 +5,7 @@ header("Access-Control-Allow-Origin: *");
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-include 'db_config.php'; // contains your DB connection details
+include 'db_config.php';
 
 $email = $_POST['email'] ?? '';
 $username = $_POST['username'] ?? '';
